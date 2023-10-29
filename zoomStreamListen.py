36c248b1df14a2fb3dd20684289d1ab42f31185c
Jacob Anderson <jacobdanderson@gmail.com>
2023-10-29 15:31:16 -0600

    Initial Commit

---
+++ zoomStreamListen.py
@@ -0,0 +1,44 @@
+# For Mac
+import cv2
+import numpy as np
+import io
+import socket
+import struct
+
+server_socket = socket.socket()
+server_socket.bind(('0.0.0.0', 8000))
+server_socket.listen(0)
+
+connection = server_socket.accept()[0].makefile('rb')
+try:
+    while True:
+        # Read the length of the image as a 32-bit unsigned int.
+        image_len_packed = connection.read(struct.calcsize('<L'))
+        if not image_len_packed:
+            break
+        image_len = struct.unpack('<L', image_len_packed)[0]
+        # Read the image data.
+        image_stream = io.BytesIO()
+        remaining = image_len
+        while remaining > 0:
+            chunk = connection.read(min(remaining, 4096))
+            if not chunk:
+                break
+            image_stream.write(chunk)
+            remaining -= len(chunk)
+        # Rewind the stream
+        image_stream.seek(0)
+
+        image = np.array(bytearray(image_stream.read()), dtype=np.uint8)
+        frame = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        # Resize the frame to the desired size for the virtual camera
+        frame = cv2.resize(frame, (1280, 720))
+        # Convert the frame to a format suitable for OS X's AVFoundation framework
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+        # Create a window to display the virtual camera
+        cv2.imshow("Virtual Camera", frame)
+        # Sleep for a short amount of time to avoid consuming too much CPU resources
+        cv2.waitKey(1)
+finally:
+    connection.close()
+    server_socket.close()
